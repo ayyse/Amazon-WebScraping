b24c8ff7e0070cc8ee2a5c31dcc4cf81a7b4a531
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-04-26 15:35:09 +0300

    first commit

---
+++ Amazon-WebScraping/Program.cs
@@ -0,0 +1,57 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon_WebScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetHtmlAsync();
+            Console.ReadLine();
+        }
+
+        private static async void GetHtmlAsync()
+        {
+            var url = "https://www.amazon.co.uk/dp/B085ZKNCSQ/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var productsHtml = htmlDocument.DocumentNode.Descendants("div")
+            .Where(node => node.GetAttributeValue("id", " ")
+            .Equals("centerCol")).ToList();
+
+            var productListItems = productsHtml[0].Descendants("div")
+                .Where(node => node.GetAttributeValue("class", " ")
+                .Equals("celwidget")).ToList();
+
+            foreach (var productListItem in productListItems)
+            {
+                // id
+                Console.WriteLine(productListItem.GetAttributeValue("id", ""));
+
+                // product name
+                Console.WriteLine("Product Name: ", productListItem.Descendants("span")
+                    .Where(node => node.GetAttributeValue("id", "")
+                    .Equals("productTitle")).FirstOrDefault().InnerText
+                    );
+
+                // product price
+                //Console.WriteLine(productListItem.Descendants("span")
+                //    .Where(node => node.GetAttributeValue("id", "")
+                //    .Equals("priceblock_saleprice")).FirstOrDefault().InnerText
+                //    );
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
