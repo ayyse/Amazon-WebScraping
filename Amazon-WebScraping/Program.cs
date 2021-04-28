62843ea7eaa074ffee8e266a4ccb7c7ca56ab00e
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-04-28 15:18:07 +0300

    url added

--- Amazon-WebScraping/Program.cs
+++ Amazon-WebScraping/Program.cs
@@ -25,16 +25,22 @@ namespace Amazon_WebScraping
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
 
-
-
             string productName = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"productTitle\"]").InnerText.Trim('\n');
             Console.WriteLine("PRODUCT NAME: " +  productName + "\n");
+
+            string visitProductStore = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"bylineInfo\"]").InnerText.Trim('\n');
+            var visitProductStoreUrl = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"bylineInfo\"]").GetAttributeValue("href", "");
+            Console.WriteLine(visitProductStore + ":" + visitProductStoreUrl);
 
             string productStar = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"acrPopover\"]").InnerText.Trim('\n');
             Console.WriteLine("PRODUCT STAR RATE: " + productStar + "\n");
 
             string productPrice = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"priceblock_saleprice\"]").InnerText.Trim('\n');
-            Console.WriteLine("PRODUCT PRICE: " + productPrice + "\n");
+            string productPriceYouSave = htmlDocument.DocumentNode.SelectSingleNode("//*[@class=\"a-span12 a-color-price a-size-base priceBlockSavingsString\"]").InnerText.Trim('\n');
+            Console.WriteLine("PRODUCT PRICE: " + productPrice + " YOU SAVE: " + productPriceYouSave + "\n");
+
+            string productNote = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"apEligibility_feature_div\"]").InnerText.Trim('\n');
+            Console.WriteLine("PRODUCT NOTE: " + productNote + "\n");
 
             string productColorBlack = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"color_name_0\"]").InnerText.Trim('\n');
             Console.WriteLine("BLACK PRODUCT PRICE: " + productColorBlack + "\n");
@@ -42,8 +48,10 @@ namespace Amazon_WebScraping
             string productColorBlue = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"color_name_1\"]").InnerText.Trim('\n');
             Console.WriteLine("BLUE PRODUCT PRICE: " + productColorBlue + "\n");
 
+            string similarProduct = htmlDocument.DocumentNode.SelectSingleNode("//*[@class=\"a-size-base a-link-normal\"]").GetAttributeValue("href", "");
+            Console.WriteLine("SIMILAR PRODUCT LINK: " + similarProduct + "\n");
 
             Console.WriteLine();
         }
     }
-}
+} 
